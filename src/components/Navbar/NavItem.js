d5820be89203575bfc4d0939be2f3f85b7c7f0fe
SinthooranR <srparamu0716@gmail.com>
2021-01-21 11:02:10 -0500

    Redux Toolkit reducers added

--- src/components/Navbar/NavItem.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-
-function NavItem(props) {
-  return <NavLink to={props.routePath}>{props.routeName}</NavLink>;
-}
-
-export default NavItem;
